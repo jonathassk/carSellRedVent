bdb40073b3d04c161f43aa93aa7c803775c9836f
joanthas <joanthas09@gmail.com>
2019-07-28 07:53:30 -0300

    commit antes de eject

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import "./App.css";
+
+export function makeTextWithMistakes(text, mistakes) {
+  // ...
+}
+
+class App extends Component {
+  renderParagraph = (paragraph, i) => {
+    return <p key={i}>{paragraph}</p>;
+  };
+
+  render() {
+    const text = "Meu texto está erado\nSegundo palagrafo";
+    const mistakes = [
+      {
+        start: 15,
+        end: 20,
+        paragraph: 0
+      },
+      {
+        start: 8,
+        end: 17,
+        paragraph: 1
+      }
+    ];
+
+    // ========================================================================
+    // Sua tarefa é implementar a função que converte as variáveis `text` e
+    // `mistakes` na variável `textWithMistakes`, para textos e erros
+    // arbitrários. Você deve fazer isso na função
+    // `makeTextWithMistakes(text, mistakes)` usando outras funções de ajuda se
+    // achar necessário
+    // ========================================================================
+
+    const textWithMistakes = [
+      ["Meu texto está ", <em>erado</em>],
+      ["Segundo ", <em>palagrafo</em>]
+    ];
+
+    return (
+      <div className="App">{textWithMistakes.map(this.renderParagraph)}</div>
+    );
+  }
+}
+
+export default App;
